4b9c70165334a13db75498e227f4b60cb7ea8577
Ian Randall <ian@xaml.geek.nz>
2012-07-06 17:21:15 +1200

    Enum support

---
+++ RtMapper.Tests/TestClasses/SourceNamespace/CustomerType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RtMapper.Tests.TestClasses.SourceNamespace
+{
+    enum CustomerType
+    {
+        Prospect,
+        Active,
+        StopCredit,
+        Cancelled
+    }
+}
